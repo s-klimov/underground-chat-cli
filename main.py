7d4ef1b05850b47b7de21742a6fd8e6b9d51fc9f
Sergei Kilmov <s.klimov@bk.ru>
2023-03-26 23:20:00 +0700

    requirements added

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import asyncio
 
+MINECHAT_HOST = 'minechat.dvmn.org'
+MINECHAT_PORT = 5000
 
-async def tcp_echo_client(message):
 
+async def tcp_echo_client():
+    """считывает сообщения из сайта в консоль"""
 
     reader, _ = await asyncio.open_connection(
-        'minechat.dvmn.org', 5000)
+        MINECHAT_HOST, MINECHAT_PORT)
 
     while data := await reader.readline():
         print(f'Received: {data.decode()!r}')
 
 
-asyncio.run(tcp_echo_client('Hello World!'))
+asyncio.run(tcp_echo_client())
