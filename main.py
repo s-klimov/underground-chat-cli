afeb935456464bb533890b94e01a6de8cef35455
Sergei Klimov <s.klimov@bk.ru>
2023-03-27 12:47:10 +0700

    Save all messages to file

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import asyncio
+import logging.config
+
+logging.config.fileConfig('logging.ini', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 
 MINECHAT_HOST = 'minechat.dvmn.org'
 MINECHAT_PORT = 5000
@@ -11,7 +15,7 @@ async def tcp_echo_client():
         MINECHAT_HOST, MINECHAT_PORT)
 
     while data := await reader.readline():
-        print(f'Received: {data.decode()!r}')
+        logger.info(data.decode().replace('\n', ''))
 
 
 asyncio.run(tcp_echo_client())
