8c261c1c10fdaf284a9bee8db90be8495bc5a4f7
Sergei Klimov <s.klimov@bk.ru>
2023-03-27 16:29:51 +0700

    Save messages history to file

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 import asyncio
 import logging.config
 
+import aiofiles as aiofiles
+from datetime import datetime as dt
+
+import backoff as backoff
+
 logging.config.fileConfig('logging.ini', disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
 MINECHAT_HOST = 'minechat.dvmn.org'
 MINECHAT_PORT = 5000
+HISTORY_FILE = 'chat.txt'
 
 
+@backoff.on_exception(backoff.expo,
+                      (OSError, asyncio.exceptions.CancelledError, asyncio.exceptions.TimeoutError))
 async def tcp_echo_client():
-    """считывает сообщения из сайта в консоль"""
+    """Считывает сообщения из сайта в консоль"""
 
     reader, _ = await asyncio.open_connection(
         MINECHAT_HOST, MINECHAT_PORT)
 
     while data := await reader.readline():
-        logger.info(data.decode().replace('\n', ''))
 
+        logger.info(data.decode().replace('\n', ''))  # логируем полученное сообщение
 
-asyncio.run(tcp_echo_client())
+        async with aiofiles.open(HISTORY_FILE, 'a') as f:  # записываем полученное сообщение в файл
+            await f.write(f"[{dt.now().strftime('%Y-%m-%d %H:%M')}]{data.decode()}")
+
+if __name__ == '__main__':
+
+    try:
+        asyncio.run(tcp_echo_client())
+    except KeyboardInterrupt:
+        logger.info("Прерываем работу сервера")  # логируем полученное сообщение
