88f9160fa828fadbf1ad5a67f339222d24b83d62
Sergei Klimov <s.klimov@bk.ru>
2023-04-23 15:55:52 +0700

    Настроена подгрузка переменных окружения

--- common.py
+++ common.py
@@ -9,6 +9,7 @@ from pathlib import Path
 
 import aiofiles
 import configargparse as configargparse
+from dotenv import load_dotenv
 
 USERS_FILE = "users.json"
 
@@ -21,11 +22,12 @@ class CommonArgs:
 
     def __init__(self):
         """Задаем общие параметры для всех скриптов проекта"""
+        load_dotenv()
 
         self._parser = configargparse.ArgParser(default_config_files=['.env', ], ignore_unknown_config_file_keys=True)
-        self._parser.add('--host', type=str, required=False, default=os.getenv('host'),
+        self._parser.add('--host', type=str, required=False, default=os.getenv('HOST'),
                          help='Хост сервера с чатом (default: %(default)s)')
-        self._parser.add('--port', type=int, required=False, default=os.getenv('port'),
+        self._parser.add('--port', type=int, required=False, default=os.getenv('PORT'),
                          help='Порт сервера с чатом (default: %(default)s)')
 
     def get_args(self):
@@ -38,7 +40,7 @@ class ListenArgs(CommonArgs):
     def __init__(self):
         """Добавляем специфические параметры командной строки для скрипта прослушивания чата"""
         super().__init__()
-        self._parser.add('--history', type=str, required=False, default=os.getenv('history'),
+        self._parser.add('--history', type=str, required=False, default=os.getenv('HISTORY'),
                          help='Путь к файлу для хранения истории чата (default: %(default)s)')
 
 
